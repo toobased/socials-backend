ddf9df5e511b640ff5e643e3e9f1f8b7449aa7a0
worlddeleteRin <worlddelete0@gmail.com>
2022-09-10 03:00:19 +0300

    ⚡improve stuff

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use axum::{
     response::IntoResponse,
     Json, Router, Extension
 };
-use socials_core::{db::{SocialsDb, DummyQuery}, tasks::{BotTaskCreate, BotTaskQuery, BotTask}};
+use socials_core::{db::{SocialsDb, DummyQuery }, tasks::{BotTaskCreate, BotTaskQuery, BotTask}, social::source::{SocialSourceQuery, SocialSource, SocialSourceCreate}};
 use std::net::SocketAddr;
 use tower::ServiceBuilder;
 use tower_http::cors::{CorsLayer, Any};
@@ -19,6 +19,8 @@ async fn main() {
         .allow_origin(Any);
 
     // init db
+    // todo
+    // let db = SocialsDb::new_test_instance().await.unwrap();
     let db = SocialsDb::new_instance().await.unwrap();
 
     let app: Router = Router::new()
@@ -34,6 +36,15 @@ async fn main() {
         )
         .route("/task_types/",
             get(get_task_types)
+        )
+        .route("/social_sources/",
+            get(get_social_sources)
+            .post(create_social_sources)
+        )
+        .route("/social_sources/:source_id",
+            get(get_social_source)
+            .delete(remove_social_sources)
+            .patch(update_social_source)
         )
         // Add middleware to all routes
         .layer(
@@ -81,10 +92,10 @@ async fn get_task(
 async fn update_task(
     Path(raw_id): Path<uuid::Uuid>,
     db: Extension<SocialsDb>,
-    Json(mut updated_task): Json<BotTask>
+    Json(updated_task): Json<BotTask>
 ) -> impl IntoResponse {
     let id = bson::Uuid::parse_str(raw_id.to_string()).unwrap();
-    let result = SocialsDb::update_by_id(id, &mut updated_task, &db.bots_tasks())
+    let result = SocialsDb::update_by_id(id, updated_task.clone(), &db.bots_tasks())
         .await.unwrap();
     Json(result)
 }
@@ -119,4 +130,67 @@ async fn get_task_types(
     let result = SocialsDb::find(&DummyQuery::default(), &db.task_types())
         .await.unwrap();
     Json(result)
+}
+
+async fn get_social_sources(
+    db: Extension<SocialsDb>
+) -> impl IntoResponse {
+    let result = SocialsDb::find(&DummyQuery::default(), &db.social_sources())
+        .await.unwrap();
+    Json(result)
+}
+
+async fn get_social_source(
+    db: Extension<SocialsDb>,
+    Path(raw_id): Path<uuid::Uuid>,
+) -> impl IntoResponse {
+    let id = bson::Uuid::parse_str(raw_id.to_string()).unwrap();
+    let result = SocialsDb::find_by_id(id, &db.social_sources())
+        .await.unwrap();
+    Json(result)
+}
+
+async fn create_social_sources(
+    db: Extension<SocialsDb>,
+    Json(source): Json<SocialSourceCreate>
+) -> impl IntoResponse {
+    let new_source = SocialSource::from(source);
+    let result = SocialsDb::insert_one(new_source, db.social_sources())
+        .await.unwrap();
+    Json(result)
+}
+
+async fn remove_social_sources(
+    db: Extension<SocialsDb>,
+    Path(raw_id): Path<uuid::Uuid>
+) -> impl IntoResponse {
+    // TODO change to find & delete when available
+    let id: bson::Uuid = bson::Uuid::parse_str(raw_id.to_string())
+        .unwrap();
+    let q = SocialSourceQuery {
+        id: Some(id),
+        ..Default::default()
+    };
+    let result = SocialsDb::delete_many(&q, &db.social_sources())
+        .await.unwrap();
+    Json(result)
+}
+
+async fn update_social_source(
+    db: Extension<SocialsDb>,
+    Path(raw_id): Path<uuid::Uuid>,
+    Json(social_source): Json<SocialSourceCreate>
+) -> impl IntoResponse {
+    let id: bson::Uuid = bson::Uuid::parse_str(raw_id.to_string())
+        .unwrap();
+
+    let mut source = SocialsDb::find_by_id(id, &db.social_sources())
+        .await.unwrap().unwrap();
+    source.update_with(social_source);
+    let res = SocialsDb::update_by_id(
+        id,
+        source,
+        &db.social_sources()
+    ).await;
+    Json(res)
 }
